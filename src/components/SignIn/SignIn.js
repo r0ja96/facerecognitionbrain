b25945e3c1d98b5a70f8bec081d32409abcde063
Alan <rodriguezalan27@gmail.com>
2021-02-28 23:57:31 -0800

    update

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -1,19 +1,58 @@
+import React,{Component} from 'react';
 import './SignIn.css';
 
-const SignIn = ({onRouteChange})=>{
+class SignIn extends React.Component{
+
+    constructor(props){
+        super(props);
+        this.state ={
+            SignInEmail:'',
+            SignInPassword: ''
+        }
+    }
+
+    onEmailChange= (event) =>{
+        this.setState({SignInEmail: event.target.value});
+    }
+
+    onPasswordChange = (event) =>{
+        this.setState({SignInPassword: event.target.value});
+    }
+
+    onSubmitSignIn = () =>{
+        console.log(this.state.SignInEmail,this.state.SignInPassword);
+        fetch('http://localhost:3000/signin',{
+            method: 'post',
+            headers: {'Content-Type':'application/json'},
+            body: JSON.stringify({
+                email: this.state.SignInEmail,
+                password: this.state.SignInPassword
+            })
+        }).then(response => response.json())
+        .then(user => {
+            if( user.id){
+            this.props.loadUser(user);    
+            this.props.onRouteChange('home');
+            }
+        });
+    }
+
+render(){
+    const {onRouteChange} = this.props;
 
     return(
         <div>
-            <form>
+            <div>
                 <label htmlFor='email'>Email</label>
-                <input id='email' type='text'/>
+                <input onChange={this.onEmailChange} id='email' type='text'/>
                 <label htmlFor='password'>Password</label>
-                <input id='password' type='text'/>
-                <input onClick={() => onRouteChange('home')} type='button' value='Sign In'/>
+                <input onChange={this.onPasswordChange} id='password' type='text'/>
+                <input onClick={this.onSubmitSignIn} type='button' value='Sign In'/>
                 <p onClick={() => onRouteChange('register')}>Register</p>
-            </form>
+            </div>
         </div>
     );
+}
 }
 
 
